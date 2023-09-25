a943ad9b13c6f584e49854eeb271e475d50424bf
juandapino <132597825+juandapino@users.noreply.github.com>
2023-09-25 11:56:05 -0500

    willi

--- streamlit1.py
+++ streamlit1.py
@@ -2,76 +2,90 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import seaborn as sns
-import matplotlib.pyplot as plt
-import mplcursors 
+import plotly.express as px
+import os
+import tempfile
+
 st.title('Pronósticos :chart_with_upwards_trend:')
 
-#traemos los datos
-st.set_option('deprecation.showPyplotGlobalUse', False) #para evitar el warning alert
+st.set_option('deprecation.showPyplotGlobalUse', False)
+
+desktop_dir = os.path.join(os.path.expanduser("~"), "Desktop")
 
 data_file = st.file_uploader("Upload XLSX", type=["XLSX"])
 
-
+# Variable para almacenar los datos originales
+datos_originales = None
 
 if data_file is not None:
-    
-  
-  datos=pd.read_excel(r'Planes.xlsx',dtype={'Año natural/Semana':str,'Material':str,'Año natural/Mes':str})
-  datos['Año natural/Semana']=datos['Año natural/Semana'].apply(str)
-  datos['Año natural/Mes']=datos['Año natural/Mes'].apply(str)
-  materiales=datos['Material'].drop_duplicates().tolist() #creamos lista de materiales
-  centros=datos['Centro'].drop_duplicates().tolist()
-  sel_mat=st.selectbox('Material', materiales)#datos['Material'].drop_duplicates) seleccionar material
-  sel_ce=st.selectbox('Centro',centros) #seleccionar centro
-  #grafica
-  sns.set(style="darkgrid",palette="pastel",rc={'figure.figsize':(12,8)})
-  fig, ax = plt.subplots()
-  #sns.lineplot(data=datos[(datos['Material']==sel_mat) & (datos['Centro']==sel_ce)],x='Año natural/Semana',y='Ajuste Plan final Línea',hue=datos['tipo_dato'],sort=True,style=datos['tipo_dato'],errorbar=None,palette=['purple','blue'], markers=True,)
-  if 'edited_df' not in st.session_state:
-      st.session_state['edited_df']=datos.copy()  
-  def update_graph():
-        ax.clear()
-        sns.lineplot(data=st.session_state['edited_df'][(st.session_state['edited_df']['Material'] == sel_mat) & (st.session_state['edited_df']['Centro'] == sel_ce)], x='Año natural/Semana',
-                 y='Ajuste Plan final Línea', hue=st.session_state['edited_df']['tipo_dato'], sort=True, style=st.session_state['edited_df']['tipo_dato'],
-                 errorbar=None, palette=['purple', 'blue'], markers=True, ax=ax)
-        sns.axes_style({'ytick.direction': 'in'})
-        
-        st.pyplot()
+    datos = pd.read_excel(data_file, dtype={'Año natural/Semana': str, 'Material': str, 'Año natural/Mes': str})
+    datos['Año natural/Semana'] = datos['Año natural/Semana'].apply(str)
+    datos['Año natural/Mes'] = datos['Año natural/Mes'].apply(str)
 
-  update_graph() 
-  #sns.axes_style({'ytick.direction': 'in'})
-  #fig=sns.mpl.pyplot.show()
-  #st.pyplot()
-  
-  cursor = mplcursors.cursor(fig, hover=True)
-  cursor.connect("add", lambda sel: st.write(f'({sel.target[0]:.2f}, {sel.target[1]:.2f})'))
-  
-  edit_df=st.data_editor(data=datos[(datos['Material']==sel_mat) & (datos['Centro']==sel_ce)],disabled=['Año natural/Semana','Mes','Año natural/Mes','Material','DescMat','Centro','DescCe','tipo_dato']
-                         ,hide_index=True,width=1080, height=400)
-  
-  col1, col2, col3 = st.columns(3)
-  
-  if 'edited_df' not in st.session_state:
-      st.session_state['edited_df']=datos.copy()
-  
-  with col1:
-      if st.button("Guardar cambios :floppy_disk:"): 
-          #edited_df=edit_df.copy()
-          st.session_state['edited_df'].update(edit_df,overwrite=True)
-          update_graph()
-          st.text("Cambios guardados🎈")
-          
-  #st.write("Contenido de edited_df:")
-  #st.write(edited_df)  # Imprime el contenido de edited_df
-  
-  with col2:
-      if st.button('Descargar cambios :printer:'):
-          #datos.update(edit_df,overwrite=True)
-          st.session_state['edited_df'].to_excel('Pronosticos.xlsx', index=False) #exporta el original con los cambios
-          del st.session_state['edited_df']
-          st.text("Descargado🎈")
+    # Realiza un seguimiento de los datos originales
+    datos_originales = datos.copy()
 
-  with col3:
-    if st.button('Borrar cambios :wastebasket:'):
-        st.session_state['edited_df']=datos.copy()
-        st.text("Cambios borrados")
+    materiales = datos['Material'].drop_duplicates().tolist()
+    centros = datos['Centro'].drop_duplicates().tolist()
+    sel_mat = st.selectbox('Material', materiales)
+    sel_ce = st.selectbox('Centro', centros)
+
+    sns.set(style="darkgrid", palette="pastel", rc={'figure.figsize': (12, 8)})
+
+    if 'edited_df' not in st.session_state:
+        st.session_state['edited_df'] = datos.copy()
+
+    def update_graph():
+        edited_df = st.session_state['edited_df']
+
+        # Filtra los datos según las selecciones de Material y Centro
+        filtered_df = edited_df[(edited_df['Material'] == sel_mat) & (edited_df['Centro'] == sel_ce)]
+
+        # Crear gráfico con plotly
+        fig = px.line(filtered_df, x='Año natural/Semana', y='Ajuste Plan final Línea', color='tipo_dato',
+                      markers=True, title=f'Pronósticos para {sel_mat} en {sel_ce}')
+
+        # Muestra los valores al pasar el cursor sobre los puntos
+        fig.update_traces(mode='lines+markers', text=filtered_df['Ajuste Plan final Línea'])
+        fig.update_traces(marker=dict(size=10))
+
+        # Actualiza el formato de las etiquetas de datos
+        fig.update_traces(hovertemplate='%{y:.2f}')
+
+        st.plotly_chart(fig)
+
+    update_graph()
+
+    edit_df = st.data_editor(data=datos[(datos['Material'] == sel_mat) & (datos['Centro'] == sel_ce)],
+                             disabled=['Año natural/Semana', 'Mes', 'Año natural/Mes', 'Material', 'DescMat', 'Centro',
+                                       'DescCe', 'tipo_dato'],
+                             hide_index=True, width=1080, height=400)
+
+    col1, col2, col3 = st.columns(3)
+
+    if 'edited_df' not in st.session_state:
+        st.session_state['edited_df'] = datos.copy()
+
+    with col1:
+        if st.button("Guardar cambios :floppy_disk:"):
+            st.session_state['edited_df'].update(edit_df, overwrite=True)
+            update_graph()
+            st.text("Cambios guardados🎈")
+
+    with col2:
+        if st.button('Descargar cambios :printer:'):
+            with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+                temp_file_path = temp_file.name
+                st.session_state['edited_df'].to_excel(temp_file_path, index=False)
+
+            st.markdown(
+                f'<a href="{temp_file_path}" download="Pronosticos.xlsx">Descargar Pronosticos.xlsx</a>',
+                unsafe_allow_html=True
+            )
+
+            st.text("Descargado🎈")
+
+    with col3:
+        if st.button('Borrar cambios :wastebasket:'):
+            st.session_state['edited_df'] = datos.copy()
+            st.text("Cambios borrados")
